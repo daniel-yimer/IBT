c434eee08a82cccfcbf9e6bf2110f75f1b7fa1b8
daniel-yimer <daniel.yimer14@gmail.com>
2026-07-09 12:22:55 +0300

    First project python bill spiliter with tip included

--- day02/spilit_bill_calculator.py
+++ day02/spilit_bill_calculator.py
@@ -1,2 +1,29 @@
+# bill spiliter with tip
+# number of people 
+# total bill price
+# tip in %
 
-def spilit_bill_calculator:
+#  make the names loop
+#
+
+namesOfCustomers =[]
+
+
+totalPrice = float(input ("enter the toal price of the bill: "))
+numberOfPeople = int(input("the number of bill to be spilit by: "))
+for i in range(numberOfPeople):
+    customerName=input("Enter the customer name: ")
+    namesOfCustomers.append(customerName)
+
+tip =  int(input("enter the amount of tip you want to give in percent: "))
+
+def spilit_bill_calculator(totalPrice , numberOfPeople, tip):
+    eachPrice = (totalPrice + (totalPrice * (tip * 1/100)))/numberOfPeople
+    return eachPrice
+
+price = spilit_bill_calculator(totalPrice , numberOfPeople, tip)
+
+print("The Total bill spilited between customers")
+
+for i in namesOfCustomers:
+    print (f"\n {i} Your total bill is {price} ETB")
